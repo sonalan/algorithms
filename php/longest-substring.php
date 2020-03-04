8579c312a65759773bcd3ea600b9665437606cd2
sonalan <sinan@onalan.net>
2020-03-03 19:20:32 -0800

    Longest Substring Without Repeating Characters

---
+++ php/longest-substring.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @param $string
+ * @return int
+ */
+function getLongestSubstring($string){
+    // check if string leng lower then 2 
+    if(strlen($string)<2) return strlen($string);
+
+
+    $hash =[];
+    $maxLenght=0;
+    $sArray = str_split($string);
+    $j=0;
+    foreach($sArray as $i=>$v){
+
+        if(array_key_exists($sArray[$i],$hash)) {
+            $j = max($j,$hash[$sArray[$i]] + 1);
+
+        }
+
+        $hash[$sArray[$i]] = $i;
+        $maxLenght = max($maxLenght, $i - $j + 1);
+        echo $i."-".$j."-".$sArray[$i]."-".$hash[$sArray[$i]]."-".$maxLenght."-".implode("",$hash)."\n";
+    }
+    return $maxLenght;
+}
+echo getLongestSubstring("abcdefghijklmnoabcdedfgabcdefabcdefghijklmnoprst");
+
+// for console run
+// php longtest-substring.php
